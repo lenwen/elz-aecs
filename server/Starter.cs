4177cf4922e2387036b6b437fa07950bb8164bd7
lenwen <lennie@lempa.se>
2015-04-13 14:12:59 +0200

    fix

--- server/Starter.cs
+++ server/Starter.cs
@@ -9,7 +9,7 @@ namespace aecs.srv.server
 {
     public class Starter
     {
-        public bool Debug { get; set; }
+        public bool DebugEnable { get; set; }
         public bool RunningModeConsole { get; set; }
 
         public bool RunningModeService { get; set; }
@@ -17,7 +17,7 @@ namespace aecs.srv.server
         public Starter()
         {
 
-            this.Debug = false;
+            this.DebugEnable = false;
             this.RunningModeConsole = false;
             this.RunningModeService = false;
         }
@@ -25,17 +25,67 @@ namespace aecs.srv.server
 
         public void Run()
         {
+            //  temp fix for debug. remove when debug class is done.
+            Debug.DebugEnable = this.DebugEnable;
 
-            Console.WriteLine("applikation is starting");
+            var runApplication = true;
+
+            Debug.Notic("Application is starting");
+            
 
             //TODO 5    Check starting values
+            #region Check starting values
+            Debug.Notic("Checking starting values");
+            if ((this.RunningModeConsole) && (this.RunningModeService))
+            {
+                runApplication = false;
+
+            }
+
+            #endregion
+
+
+            if (runApplication)
+            {
+                
+            }
+
+            //System.Threading.Thread.CurrentThread.Abort();
+            //if (this.Debug)
+            //    Console.WriteLine("FEL....");
+
 
             //TODO  5   Check configurationfile
 
             //TODO 5    Start applikation
-            
+
+
+
+
+
         }
 
+        public static class  Debug
+        {
+            public static bool DebugEnable { get;set; }
+            
+            public static void Error(string text)
+            {
+                if (DebugEnable)
+                    Console.WriteLine(DateTime.Now +  " - Starter - " + text);
+            }
+            public static void Warning(string text)
+            {
+                if (DebugEnable)
+                    Console.WriteLine(DateTime.Now + " - Starter - " + text);
+            }
+            public static void Notic(string text)
+            {
+                if (DebugEnable)
+                    Console.WriteLine(DateTime.Now + " - Starter - " + text);
+            }
+             
+        }
         
     }
 }
